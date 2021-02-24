0011dd198fb596f1c95c3d056dc32579d32bd49b
pavan1645 <pavan1645@gmail.com>
2021-02-23 21:18:38 -0800

    returning only body

--- server/index.js
+++ server/index.js
@@ -8,16 +8,12 @@ exports.handler = async (event) => {
   baseUrl.search = urlSearchParams;
   
   const res = await (await fetch(baseUrl.toString())).json();
-  const body = res.map(photo => ({
+  const response = res.map(photo => ({
     id: photo.id,
     image: photo.urls.full,
     name: photo.user.name,
     link: photo.user.links.html
   }))
   
-  const response = {
-      statusCode: 200,
-      body: body,
-  };
   return response;
 };
